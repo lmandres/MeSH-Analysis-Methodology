63b6936ce0196722611f429faabc51731f9db668
lmandres <lmandres@users.noreply.github.com>
2015-12-10 17:47:45 -0500

    Added code for --license option

--- update_mesh_descriptors.py
+++ update_mesh_descriptors.py
@@ -106,5 +106,31 @@ def run_mesh_update(pyodbc_conn_string):
     fileIn.close()
 
 if __name__ == '__main__':
+    
+    try:
+        if sys.argv[1] == '--license':
+            print('''
+update_mesh_descriptors.py
+Script that creates automatic searches on PubMed.
+Copyright (C) 2015  Leo Andres
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Leo Andres
+P. O. Box 3174
+Decatur, GA 30031
+lmandres@yahoo.com\n''')
+    except IndexError as ie:
 	run_mesh_update('DRIVER={Microsoft Access Driver (*.mdb, *.accdb)}; DBQ=D:/justme/VirtualBox Shared Folder/CEISMC Work/CEISMC Publications SNA/pubmed_db_-_ACTSI_investigator_collaborations_-_2015-03-18.accdb;')
 							
