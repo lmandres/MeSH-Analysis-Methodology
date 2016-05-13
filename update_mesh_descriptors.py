6377faadc9b5dc015ba802ee33d02bfad0c17278
lmandres <lmandres@users.noreply.github.com>
2016-05-13 19:20:29 -0400

    Update update_mesh_descriptors.py

--- update_mesh_descriptors.py
+++ update_mesh_descriptors.py
@@ -70,101 +70,101 @@ class PubMedSearchApp:
                 else:
                     raise per
 	    
-	    elif self.search_settings.get_database_connection_type().upper() == 'PYODBCDRIVER':
-	    	
-	    	import pyodbc
+		elif self.search_settings.get_database_connection_type().upper() == 'PYODBCDRIVER':
+
+			import pyodbc
 	            
-		    try:
-		        self.pubmed_database.database_manager.database_cursor.execute("DROP TABLE tbl_Mesh_Descriptor_Tree_Numbers;")
-		        self.pubmed_database.database_manager.database_cursor.commit()
-		        print('Dropped table: tbl_Mesh_Descriptor_Tree_Numbers')
-		    except pyodbc.ProgrammingError as per:
-		        if per.args[0] == '42S02':
-		            print('Drop table skipped: tbl_Mesh_Descriptor_Tree_Numbers')
-		        else:
-		            raise per
-		    except pyodbc.Error as er:
-		        if er.args[0] == 'HY109':
-		            print('Drop table skipped: tbl_Mesh_Descriptor_Tree_Numbers')
-		        else:
-		            raise er
+			try:
+				self.pubmed_database.database_manager.database_cursor.execute("DROP TABLE tbl_Mesh_Descriptor_Tree_Numbers;")
+				self.pubmed_database.database_manager.database_cursor.commit()
+				print('Dropped table: tbl_Mesh_Descriptor_Tree_Numbers')
+			except pyodbc.ProgrammingError as per:
+				if per.args[0] == '42S02':
+					print('Drop table skipped: tbl_Mesh_Descriptor_Tree_Numbers')
+				else:
+					raise per
+			except pyodbc.Error as er:
+				if er.args[0] == 'HY109':
+					print('Drop table skipped: tbl_Mesh_Descriptor_Tree_Numbers')
+				else:
+					raise er
 		        
-		    try:
-		        self.pubmed_database.database_manager.database_cursor.execute("""
+			try:
+				self.pubmed_database.database_manager.database_cursor.execute("""
 		                                        CREATE TABLE tbl_Mesh_Descriptor_Tree_Numbers (
 		                                            Mesh_Descriptor_Tree_Number_ID COUNTER PRIMARY KEY,
 		                                            Mesh_Descriptor_ID INTEGER,
 		                                            Mesh_Descriptor_Tree_Number TEXT
 		                                        );""")
-		        self.pubmed_database.database_manager.database_cursor.commit()
-		        print('Created table: tbl_Mesh_Descriptor_Tree_Numbers')
-		    except pyodbc.ProgrammingError as per:
-		        if per.args[0] == '42S01':
-		            print('Create table skipped: tbl_Mesh_Descriptor_Tree_Numbers')
-		        else:
-		            raise per
+				self.pubmed_database.database_manager.database_cursor.commit()
+				print('Created table: tbl_Mesh_Descriptor_Tree_Numbers')
+			except pyodbc.ProgrammingError as per:
+				if per.args[0] == '42S01':
+					print('Create table skipped: tbl_Mesh_Descriptor_Tree_Numbers')
+				else:
+					raise per
 	    
-	    fileIn = open('desc2015.xml', 'r')
-	
-	    xmlObject = searchlib.helper.TextXMLParser()
-	
-	    xmlOpenRegEx = re.compile('(?!<DescriptorRecordSet.*?>)(<DescriptorRecord.*?>.*)')
-	    xmlCloseRegEx = re.compile('(.*</DescriptorRecord>)')
+		fileIn = open('desc2015.xml', 'r')
+		
+		xmlObject = searchlib.helper.TextXMLParser()
+		
+		xmlOpenRegEx = re.compile('(?!<DescriptorRecordSet.*?>)(<DescriptorRecord.*?>.*)')
+		xmlCloseRegEx = re.compile('(.*</DescriptorRecord>)')
 	        
-	    for fileLine in fileIn:
+		for fileLine in fileIn:
 	
-	        if xmlReadStage == 1:
+			if xmlReadStage == 1:
 	            
-	            match = xmlOpenRegEx.search(fileLine)
-	            if match:
-	                xmlString = match.group(1)
-	                xmlReadStage += 1
+				match = xmlOpenRegEx.search(fileLine)
+				if match:
+					xmlString = match.group(1)
+					xmlReadStage += 1
 	        
-	        elif xmlReadStage == 2:
+			elif xmlReadStage == 2:
 	
-	            match = xmlCloseRegEx.search(fileLine)
-	            if not match:
-	                xmlString += fileLine
-	            else:
+				match = xmlCloseRegEx.search(fileLine)
+				if not match:
+					xmlString += fileLine
+				else:
 	                
-	                xmlString += match.group(1)
+					xmlString += match.group(1)
+					
+					httpStatus = 0
+					
+					xmlObject.parse_xml_string(xmlString.encode('utf-8'))
+					xmlStringData = xmlString
 	
-	                httpStatus = 0
-	                    
-	                xmlObject.parse_xml_string(xmlString.encode('utf-8'))
-	                xmlStringData = xmlString
-	
-	                print(
+					print(
 	                    xmlObject.get_element_item('<DescriptorRecord><DescriptorUI>')['character_data'][0].strip() + ' = ' +
 	                    xmlObject.get_element_item('<DescriptorRecord><DescriptorName><String>')['character_data'][0].strip())
 	
-	                rows = self.pubmed_database.database_manager.database_cursor.execute("SELECT Mesh_Descriptor_ID FROM tbl_Mesh_Descriptors WHERE Mesh_Descriptor = ?;", (
+					rows = self.pubmed_database.database_manager.database_cursor.execute("SELECT Mesh_Descriptor_ID FROM tbl_Mesh_Descriptors WHERE Mesh_Descriptor = ?;", (
 	                    xmlObject.get_element_item('<DescriptorRecord><DescriptorName><String>')['character_data'][0].strip())).fetchall()
 	
-	                if len(rows):
+					if len(rows):
 	
-	                    for row in rows:
+						for row in rows:
 	
-	                        if row[0]:
+							if row[0]:
 	                            
-	                            treeNumberIndex = 0
+								treeNumberIndex = 0
 	                            
-	                            self.pubmed_database.database_manager.database_cursor.execute("UPDATE tbl_Mesh_Descriptors SET DescriptorUI = ? WHERE Mesh_Descriptor_ID = ?;", (
-	                                xmlObject.get_element_item('<DescriptorRecord><DescriptorUI>')['character_data'][0].strip(),
-	                                str(row[0])))
-	                            self.pubmed_database.database_manager.database_cursor.commit()
+								self.pubmed_database.database_manager.database_cursor.execute("UPDATE tbl_Mesh_Descriptors SET DescriptorUI = ? WHERE Mesh_Descriptor_ID = ?;", (
+									xmlObject.get_element_item('<DescriptorRecord><DescriptorUI>')['character_data'][0].strip(),
+									str(row[0])))
+								self.pubmed_database.database_manager.database_cursor.commit()
 	
-	                            while xmlObject.get_element_item('<DescriptorRecord><TreeNumberList><TreeNumber<' + str(treeNumberIndex).strip() + '>>'):
+								while xmlObject.get_element_item('<DescriptorRecord><TreeNumberList><TreeNumber<' + str(treeNumberIndex).strip() + '>>'):
 	                                
-	                                self.pubmed_database.database_manager.database_cursor.execute("INSERT INTO tbl_Mesh_Descriptor_Tree_Numbers (Mesh_Descriptor_ID, Mesh_Descriptor_Tree_Number) VALUES (?, ?);", (
-	                                    str(row[0]),
-	                                    xmlObject.get_element_item('<DescriptorRecord><TreeNumberList><TreeNumber<' + str(treeNumberIndex).strip() + '>>')['character_data'][0].strip()))
+									self.pubmed_database.database_manager.database_cursor.execute("INSERT INTO tbl_Mesh_Descriptor_Tree_Numbers (Mesh_Descriptor_ID, Mesh_Descriptor_Tree_Number) VALUES (?, ?);", (
+										str(row[0]),
+										xmlObject.get_element_item('<DescriptorRecord><TreeNumberList><TreeNumber<' + str(treeNumberIndex).strip() + '>>')['character_data'][0].strip()))
 	                                
-	                                treeNumberIndex += 1
+									treeNumberIndex += 1
 	                            
-	                xmlReadStage = 1
+					xmlReadStage = 1
 	
-	    fileIn.close()
+		fileIn.close()
 
 if __name__ == '__main__':
     
@@ -195,4 +195,3 @@ lmandres@yahoo.com\n''')
     except IndexError as ie:
         pubmed_search = PubMedSearchApp()
         pubmed_search.run_mesh_update()
-							
